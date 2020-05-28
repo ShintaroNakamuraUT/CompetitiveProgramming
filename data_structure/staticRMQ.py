be229c58d421f0c6c36ba04f9e4bff64367d8fef
ShintaroNakamuraUT <nakamurashintaro@gmail.com>
2020-05-28 12:05:37 +0900

    'staticRMQ'

---
+++ data_structure/staticRMQ.py
@@ -0,0 +1,38 @@
+# staticRMQ = "static range minimum query'
+
+class RMQ:
+    def __init__(self, arr, INF=1 << 63):
+        self.n = n = len(arr)
+        self.double = d = [list(arr)]
+        self.INF = INF
+        ii = 1
+        while ii < n:
+            row = d[-1].copy()
+            for j in range(n - ii):
+                row[j] = min(row[j], row[j + ii])
+            d.append(row)
+            ii <<= 1
+
+    def get(self, l, r):
+        """ min value of [l, r) """
+        k = r - l
+        i = 0
+        j = l
+        d = self.double
+        result = self.INF
+        while k:
+            if k & 1:
+                result = min(result, d[i][j])
+                j += 1 << i
+            i += 1
+            k >>= 1
+        return result
+
+
+# n, q = map(int, input().split())
+# l = list(map(int, input().split()))
+# dbl = RMQ(l)
+# for _ in range(q):
+#     l,r = map(int, input().split())
+#     print(dbl.get(l,r))
+
